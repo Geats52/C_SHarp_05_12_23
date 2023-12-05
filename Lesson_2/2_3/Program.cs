7cad129c9c9b5f7a02dca455ffad3199e93d1469
Kirill <borisenkobak@yandex.ru>
2023-12-05 15:52:13 +1000

    Classwork 2_3

---
+++ Lesson_2/2_3/Program.cs
@@ -0,0 +1,10 @@
+﻿//Программа, которая принамет на вход 2 числа и выводит, является ли второе число кратным первому.
+//Если второе число некратно первому, то программа выводит остаток от деления
+
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+
+if (a % b == 0)
+    Console.WriteLine("Multiply");
+else
+    Console.WriteLine($"Multiply {a} % {b} = {a % b}");
