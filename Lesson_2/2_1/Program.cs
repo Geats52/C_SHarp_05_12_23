308b335c78627c5bf0a0fc9a13ddcfd4e5192d43
Kirill <borisenkobak@yandex.ru>
2023-12-05 15:42:32 +1000

    Classwork 2_2

--- Lesson_2/2_1/Program.cs
+++ Lesson_2/2_1/Program.cs
@@ -1,2 +1,3 @@
-﻿int n = int.Parse(Console.ReadLine()!);
+﻿//Программа, кторая вытаскивает 2 число из трёхзначного числа, делая его двухзначным
+int n = int.Parse(Console.ReadLine()!);
 Console.WriteLine(n / 100 * 10 + n % 10);
