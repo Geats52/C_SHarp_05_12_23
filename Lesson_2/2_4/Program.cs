45579927b8fa19b8fd3d156354c721ca828c9dea
Kirill <borisenkobak@yandex.ru>
2023-12-05 16:04:09 +1000

    Classwork 2_4

---
+++ Lesson_2/2_4/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая выводит третью с конца цифру заданного числа  или сообщает, что третьей цифры нет
+
+void ThirdNum(int num)
+{
+    if (num < 100)
+    {
+        Console.WriteLine("There is no digit");
+        return;
+    }
+//    while(num > 999)
+//        num /= 10;
+    Console.WriteLine(num % 10);
+}
+
+int a = int.Parse(Console.ReadLine()!);
+ThirdNum(a);
