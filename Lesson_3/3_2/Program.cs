e14fc3ac31da7b06d3509566f927e3df2f5b8c63
Kirill <borisenkobak@yandex.ru>
2023-12-07 11:22:14 +1000

    Task 3_2

---
+++ Lesson_3/3_2/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача: Задайте массив из 10 элементов, заполненый числами из промежутка [-10, 10]. Замените отрицательные элементы на положительные, а положительные на отрицательные
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(from, to + 1);
+    }
+    return arr;
+}
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void ChangeValue(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = -arr[i];
+    }
+}
+
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = MassNums(num, start, stop);
+Print(mass);
+ChangeValue(mass);
+Print(mass);
