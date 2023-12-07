6263191c65a5cc2e6662eee8b435428644cb8c49
Kirill <borisenkobak@yandex.ru>
2023-12-07 11:53:30 +1000

    Task 3_4*

---
+++ Lesson_3/3_4/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задание *: Дано натуральное трёхзначное число. Создайте массив, состоящий из цифр этого числа. Младший разряд числа должен располагаться на 0-м индексе масствва, старший - на 2-м.
+
+int[] MassNums(int num)
+{
+    int[] arr = new int[3];
+
+    for (int i = 0; i < 3; i++)
+    {
+        arr[i] = num % 10;
+        num /= 10;
+    }
+    return arr;
+}
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int number = new Random().Next(100, 1000);
+Console. WriteLine(number);
+int[] mass = MassNums(number);
+Print(mass);
